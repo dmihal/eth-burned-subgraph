f8253be2fea14b8dadb37b12fccc5722997c69e5
David Mihal <david@dmihal.com>
2021-08-01 14:53:14 +0100

    Create subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,3 +1,20 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { Contract } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { BigInt, ethereum } from "@graphprotocol/graph-ts"
+import { ETHBurned } from "../generated/schema"
+
+let EIGHTEEN_DECIMALS = BigInt.fromI32(10).pow(18).toBigDecimal()
+
+export function handleBlock(block: ethereum.Block): void {
+  let entity = ETHBurned.load('1')
+  if (!entity) {
+    entity = new ETHBurned('1')
+    entity.burned = BigInt.fromI32(0).toBigDecimal()
+  }
+
+  if (block.baseFeePerGas != null) {
+    let baseFee = block.baseFeePerGas as BigInt
+    let burned = (block.gasUsed.times(baseFee)).divDecimal(EIGHTEEN_DECIMALS)
+    entity.burned += burned
+
+    entity.save();
+  }
+}
